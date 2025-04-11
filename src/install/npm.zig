158312a2430d24067ee6031bcf2d00d3cdbb4927
José Miguel Sarasola <jmsarasola@irontec.com>
2025-04-11 09:41:15 +0200

    Use the Kubernetes Verdaccio as default npm registry

--- src/install/npm.zig
+++ src/install/npm.zig
@@ -242,7 +242,7 @@ pub fn responseError(
 }
 
 pub const Registry = struct {
-    pub const default_url = "https://registry.npmjs.org/";
+    pub const default_url = "http://verdaccio.verdaccio.svc.cluster.local:4873/";
     pub const default_url_hash = bun.Wyhash11.hash(0, strings.withoutTrailingSlash(default_url));
     pub const BodyPool = ObjectPool(MutableString, MutableString.init2048, true, 8);
 
